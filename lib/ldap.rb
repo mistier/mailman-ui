20568d47e0d5dc8daf6ef4defdb46f1b078701a1
Kento Nomiyama <nonoca@klab.ai.kyutech.ac.jp>
2016-05-17 22:29:19 +0900

    defined getML for LDAP return Array or false
    including the execution of this code

--- lib/ldap.rb
+++ lib/ldap.rb
@@ -1,30 +1,51 @@
 require 'rubygems'
 require 'net/ldap'
 
-SERVER = 'example.com'
-PORT = 389
-BASE = 'ou=People,dc=example,dc=com'
+$SERVER = 'example.com'
+$PORT = 389
+$DN ='dc=example,dc=com'
+$BASE = 'ou=People,'.concat($DN)
+$PASSWORD = 'password'
 
-conn = Net::LDAP.new :host => SERVER,
-                     :port => PORT,
-                     :auth => {
-                         :method => :simple,
-                         :username => "cn=Manager,dc=example,dc=com",
-                         :password => "password"
-                     }
+def getML(username)
 
-conn.open do |ldap|
-  treebase = BASE
-  filter = Net::LDAP::Filter.eq( "uid", ARGV[0] )
-  attrs = ["title"]
-  ldap.search(:base => treebase, :filter => filter, :attributes => attrs, :return_result => false ) do |entry|
-    entry.each do |attr, values|
-      values.each do |value|
-        if /^ml-/ =~ value
-          ml = value.sub(/^ml-/,"").sub(/-admin$/,"")
-          puts "#{ml}"
+  # ml list
+  mllist = []
+  # Connet to LDAP
+  conn = Net::LDAP.new :host => $SERVER,
+                       :port => $PORT,
+                       :auth => {
+                           :method => :simple,
+                           :username => "cn=Manager,".concat($DN),
+                           :password => $PASSWORD
+                       }
+
+  conn.open do |ldap|
+    treebase = $BASE
+    filter = Net::LDAP::Filter.eq("uid", username)
+    attrs = ["title"]
+    ldap.search(:base => treebase, :filter => filter, :attributes => attrs, :return_result => false ) do |entry|
+      entry.each do |attr, values|
+        values.each do |value|
+          if /^ml-/ =~ value then
+            ml = value.sub(/^ml-/,"").sub(/-admin$/,"")
+            mllist << ml
+          elsif /^uid=/ =~ value then
+          else
+          end
         end
       end
     end
   end
+
+  # check ml
+  if mllist.length > 0 then
+    return mllist
+  else
+    return false
+  end
+
 end
+
+# example
+mail = getML(username)
