69f96f75bf30006b636d6c73dce52cc73aad9112
Kento Nomiyama <nonoca@klab.ai.kyutech.ac.jp>
2016-05-17 22:51:33 +0900

    LDAPAction を module 化
    the execution of this code
    
    ```
    mail = Action.new
    mail.setUser(ARGV[0])
    mail.listML
    p mail.getMailingList
    ```

--- lib/ldap.rb
+++ lib/ldap.rb
@@ -7,45 +7,65 @@ $DN ='dc=example,dc=com'
 $BASE = 'ou=People,'.concat($DN)
 $PASSWORD = 'password'
 
-def getML(username)
+module LDAPAction
+  def getML(username)
 
-  # ml list
-  mllist = []
-  # Connet to LDAP
-  conn = Net::LDAP.new :host => $SERVER,
-                       :port => $PORT,
-                       :auth => {
-                           :method => :simple,
-                           :username => "cn=Manager,".concat($DN),
-                           :password => $PASSWORD
-                       }
+    # ml list
+    mllist = []
+    # Connet to LDAP
+    conn = Net::LDAP.new :host => $SERVER,
+                         :port => $PORT,
+                         :auth => {
+                             :method => :simple,
+                             :username => "cn=Manager,".concat($DN),
+                             :password => $PASSWORD
+                         }
 
-  conn.open do |ldap|
-    treebase = $BASE
-    filter = Net::LDAP::Filter.eq("uid", username)
-    attrs = ["title"]
-    ldap.search(:base => treebase, :filter => filter, :attributes => attrs, :return_result => false ) do |entry|
-      entry.each do |attr, values|
-        values.each do |value|
-          if /^ml-/ =~ value then
-            ml = value.sub(/^ml-/,"").sub(/-admin$/,"")
-            mllist << ml
-          elsif /^uid=/ =~ value then
-          else
+    conn.open do |ldap|
+      treebase = $BASE
+      filter = Net::LDAP::Filter.eq("uid", username)
+      attrs = ["title"]
+      ldap.search(:base => treebase, :filter => filter, :attributes => attrs, :return_result => false ) do |entry|
+        entry.each do |attr, values|
+          values.each do |value|
+            if /^ml-/ =~ value then
+              ml = value.sub(/^ml-/,"").sub(/-admin$/,"")
+              mllist << ml
+            elsif /^uid=/ =~ value then
+            else
+            end
           end
         end
       end
     end
+
+    # check ml
+    if mllist.length > 0 then
+      return mllist
+    else
+      return false
+    end
+  end
+end
+
+class Action
+  include LDAPAction
+
+  def initialize()
+    @user = ''
+    @ml = []
   end
 
-  # check ml
-  if mllist.length > 0 then
-    return mllist
-  else
-    return false
+  def setUser(username)
+    @user = username
+  end
+
+  def listML()
+   @ml = getML(@user)
+  end
+
+  def getMailingList()
+    return @ml
   end
 
 end
-
-# example
-mail = getML(username)
