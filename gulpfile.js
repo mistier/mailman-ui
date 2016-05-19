d032415fc44b59ef19ce19bb90394c593f6d2edc
Naohiro IWAMOTO <mayok@club.kyutech.ac.jp>
2016-05-19 16:38:32 +0900

    gulp-webserver を追加した

--- gulpfile.js
+++ gulpfile.js
@@ -2,10 +2,9 @@ var gulp = require('gulp');
 var sass = require('gulp-sass');
 var autoprefixer = require('gulp-autoprefixer');
 var plumber = require('gulp-plumber');
+var webserver = require('gulp-webserver');
 
-gulp.task('default', function() {
-  gulp.watch("scss/**/*.scss", ["sass"]);
-});
+gulp.task('default', ['watch', 'server']);
 
 gulp.task('sass', function() {
   gulp.src("scss/style.scss")
@@ -13,5 +12,15 @@ gulp.task('sass', function() {
     .pipe(sass())
     .pipe(autoprefixer())
     .pipe(gulp.dest("./css"));
+});
 
+gulp.task('watch', function() {
+  gulp.watch('scss/*.scss', ['sass']);
+});
+
+gulp.task('server', function() {
+  gulp.src('.')
+    .pipe(webserver({
+      livereload: true
+    }));
 });
